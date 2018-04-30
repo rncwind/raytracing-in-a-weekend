b39c0d0ac9dfa6bd9ab2350a4f7ae9a5c3369628
rncwind <3758541+rncwind@users.noreply.github.com>
2018-04-30 21:05:07 +0100

    Finish ch2

--- ch2/main.cpp
+++ ch2/main.cpp
@@ -1,8 +1,21 @@
 #include "vec3.h"
+#include <iostream>
 
 int main()
 {
-    vec3 v1(5,5,5);
-    vec3 v2(10,10,10);
-    vec3 v3 = v1 + v2;
+    int nx = 200;
+    int ny = 100;
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    for(int j = ny-1; j >= 0; j--)
+    {
+        for(int i = 0; i < nx; i++)
+        {
+            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+            int ir = int(255.99*col[0]);
+            int ig = int(255.99*col[1]);
+            int ib = int(255.99*col[2]);
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+    return 0;
 }
